d1d1b9ac1f85ab81fcc17ede31b89203b7f6e416
David Danziger <ddanziger@gmail.com>
2024-05-20 17:10:35 +0000

    complete exercise

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 let button = document.querySelector("button");
                                     
 // 1. Declare a variable called `answer` that stores the p tag that has the class name `answer`.
-
-
+// let answer = document.querySelector("p.answer");
+let answer = document.querySelector("p.answer");
 
 button.addEventListener("click", function() {
+  // console.log('test');
 // 2. Replace the value with a string with a name of a movie you enjoy.
 // 3. Uncomment the line of code below, run, and try the button.
-// answer.innerHTML="movie name goes here";
+answer.innerText="Annie Hall";
 
 
 
